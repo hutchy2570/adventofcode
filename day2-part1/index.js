095e75f4a3c24e28aa5ef5d0c6bc48e5773db415
Mike Hutchinson <mike.hutchinson@infinityworks.com>
2020-12-02 10:05:16 +0000

    Day2

---
+++ day2-part1/index.js
@@ -0,0 +1,30 @@
+const { SSL_OP_ALL } = require('constants');
+const fs = require('fs')
+
+const inputPattern = new RegExp('(\\d+)-(\\d+) (\\w): (\\w+)');
+
+const run = () => {
+    const input = fs.readFileSync('day2-part1/input.txt').toString().split('\n').map((s) => {
+        const [, lower, upper, char, password] = s.match(inputPattern)
+        return {
+            lower,
+            upper,
+            char,
+            password
+        }
+    })
+
+    let valid = 0
+
+    input.forEach(({lower, upper, char, password}) => {
+        const numOccurrances = (password.match(new RegExp(char, 'g')) || []).length
+
+        if (numOccurrances >= lower && numOccurrances <= upper) {
+            valid += 1
+        }
+    })
+
+    console.log(valid)
+}
+
+run()
---
+++ day2-part2/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+const inputPattern = new RegExp('(\\d+)-(\\d+) (\\w): (\\w+)');
+
+const run = () => {
+    const input = fs.readFileSync('day2-part1/input.txt').toString().split('\n').map((s) => {
+        const [, first, second, char, password] = s.match(inputPattern)
+        return {
+            first,
+            second,
+            char,
+            password
+        }
+    })
+
+    const valid = input.filter(({first, second, char, password}) => {
+        const hasFirst = password.charAt(first - 1) === char
+        const hasSecond = password.charAt(second - 1) === char
+
+        return !(hasFirst && hasSecond) && (hasFirst || hasSecond)
+    })
+
+    console.log(valid.length)
+}
+
+run()
